4c3713e72b8ac2be4f3f2942e0889ecbe544872f
DevTunechi <tunjidare2@yahoo.com>
2025-08-24 18:41:47 +0100

    8080 app.listen

--- wealthrun-backend/src/server.js
+++ wealthrun-backend/src/server.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 // ------------------------
 // Start server
 // ------------------------
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
